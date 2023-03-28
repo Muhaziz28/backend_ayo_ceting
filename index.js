11db4f8e942afae11303af35a105116b2e864635
haikal <haikal@haikals-MacBook-Pro.local>
2023-03-28 14:26:24 +0700

    add unit test

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ dotenv.config();
 const app = express()
 
 app.use(express.json())
+
 app.use(RolesRoute)
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server is running on port ${process.env.APP_PORT}...`)
 })
-
